d3d03bc6b1774e2e9b680599aebd115d61ae3acb
Anirudh Natarajan <anirudhnatarajan@Anirudhs-MBP.attlocal.net>
2017-01-08 15:44:59 -0800

    controller

---
+++ Controller/ControllerManager.swift
@@ -0,0 +1,81 @@
+//
+//  ControllerManager.swift
+//  Controller
+//
+//  Created by Anirudh Natarajan on 12/4/16.
+//  Copyright © 2016 Kodikos. All rights reserved.
+//
+
+import Foundation
+
+class ControllerManager {
+    let redisServer = Redis()
+    let ip = "10.152.2.12"
+    let port = 6379
+    var playerID: Int!
+    
+    
+    init() {
+        connect()
+        redisServer.Command(Command: "LPUSH list 0")
+        redisServer.Command(Command: "LPUSH list 0")
+        
+    }
+    
+    private func connect() {
+        redisServer.server(endPoint: ip, onPort: UInt16(port))
+    }
+    
+    public func setPlayer(player:Int){
+        playerID = player
+    }
+    
+    class var sharedInstance: ControllerManager {
+        struct Static {
+            static let instance = ControllerManager()
+        }
+        return Static.instance
+    }
+    
+    func sendData(x: Double){
+        if(playerID==1){
+            redisServer.Command(Command: "LSET list 0 \(x)")
+        }
+        else if(playerID==2){
+            redisServer.Command(Command: "LSET list 1 \(x)")
+        }
+    }
+    
+    func moveUp() {
+        if(playerID==1){
+            redisServer.Command(Command: "LSET list 0 1")
+        }
+        else if(playerID==2){
+            redisServer.Command(Command: "LSET list 1 1")
+        }
+    }
+    
+    func moveDown(){
+        if(playerID==1){
+            redisServer.Command(Command: "LSET list 0 -1")
+        }
+        else if(playerID==2){
+            redisServer.Command(Command: "LSET list 1 -1")
+        }
+    }
+    
+    func stopMoving(){
+        if(playerID==1){
+            redisServer.Command(Command: "LSET list 0 0")
+        }
+        else if(playerID==2){
+            redisServer.Command(Command: "LSET list 1 0")
+        }
+    }
+    
+    func player(x:Int){
+        redisServer.Command(Command: "SET player \(x)")
+        
+    }
+    
+}
---
+++ Controller/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Controller
+//
+//  Created by Anirudh Natarajan on 12/4/16.
+//  Copyright © 2016 Kodikos. All rights reserved.
+//
+
+import UIKit
+import CocoaAsyncSocket
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var playerLabel: UILabel!
+    
+    var playerID:Int?
+//    var motionManager: CMMotionManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        playerLabel.text = "Player \(playerID!)"
+    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        motionManager = CMMotionManager()
+//        motionManager.startAccelerometerUpdates()
+//        while(true){
+//            if let accelerometerData = motionManager.accelerometerData {
+//                print(accelerometerData.acceleration.x)
+//                ControllerManager.sharedInstance.sendData(x: accelerometerData.acceleration.x)
+//            }
+//        }
+//    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func upPressed(_ sender: Any) {
+        ControllerManager.sharedInstance.stopMoving()
+    }
+    @IBAction func upReleased(_ sender: Any) {
+        ControllerManager.sharedInstance.moveUp()
+    }
+    
+    @IBAction func downPressed(_ sender: Any) {
+        ControllerManager.sharedInstance.stopMoving()
+    }
+    @IBAction func downReleased(_ sender: Any) {
+        ControllerManager.sharedInstance.moveDown()
+    }
+    
+    
+}
+
